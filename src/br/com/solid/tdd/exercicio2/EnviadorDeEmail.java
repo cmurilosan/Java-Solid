e8108e7da8e539c4362d72aeb606a4aefb89cd8c
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-07 16:03:32 -0300

    Aula 02 - Acoplamento e Estabilidade 01

---
+++ src/br/com/solid/tdd/exercicio2/EnviadorDeEmail.java
@@ -0,0 +1,8 @@
+package br.com.solid.tdd.exercicio2;
+
+public class EnviadorDeEmail {
+
+    public void enviaEmail(NotaFiscal nf) {
+        System.out.println("envia email da nf " + nf.getId());
+    }
+}
---
+++ src/br/com/solid/tdd/exercicio2/NotaFiscalDao.java
@@ -0,0 +1,8 @@
+package br.com.solid.tdd.exercicio2;
+
+public class NotaFiscalDao {
+
+    public void persiste(NotaFiscal nf) {
+        System.out.println("salva nf no banco");
+    }
+}
