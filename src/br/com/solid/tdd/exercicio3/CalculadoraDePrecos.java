2a3fc625839e47fd097dda17e220190bbb26cc2f
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-08 14:31:48 -0300

    Aula 03 - Classes abertas, Open Closed e Dependency Inversion Principles 01

---
+++ src/br/com/solid/tdd/exercicio3/CalculadoraDePrecos.java
@@ -0,0 +1,15 @@
+package br.com.solid.tdd.exercicio3;
+
+public class CalculadoraDePrecos {
+
+    public double calcula(Compra produto) {
+        TabelaDePrecoPadrao tabela = new TabelaDePrecoPadrao();
+        Frete correios = new Frete();
+
+        double desconto = tabela.descontoPara(produto.getValor());
+        double frete = correios.para(produto.getCidade());
+
+        return produto.getValor() * (1-desconto) + frete;
+    }
+
+}
